5338d93e5ff076dbe9afbfcb7038082f95ea9d79
Elijah Mylcev <elijahmylcev@gmail.com>
2022-01-17 23:06:50 +0300

    working algorithm for spiral

--- spiral/index.js
+++ spiral/index.js
@@ -2,13 +2,48 @@ const width = +prompt('Ширина матрицы');
 const height = +prompt('Высота матрицы');
 
 function matrix(width, height) {
-	let result = new Array(height).fill().map(() => new Array(width));
+	let result = new Array(height).fill().map(() => new Array(width).fill(''));
+
+	let counter = 1;
+	let startCol = 0;
+	let endCol = width - 1;
+	let startRow = 0;
+	let endRow = height - 1;
+
+	while (startCol <= endCol && startRow <= endRow) {
+		for (let i = startCol; i <= endCol; i++) {
+			result[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+
+		for (let j = startRow; j <= endRow; j++) {
+			result[j][endCol] = counter;
+			counter++;
+		}
+		endCol--;
+
+		for (let i = endCol; i >= startCol; i--) {
+			result[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		for (let i = endRow; i >= startRow; i--) {
+			result[i][startCol] = counter;
+			counter++;
+		}
+		startCol++;
+	}
 
 	return result;
 }
 
-[
-	[1, 2, 3],
-	[8, 9, 4],
-	[7, 6, 5],
-];
+console.log(matrix(width, height));
+
+// matrix
+// [
+// 	[1, 2, 3],
+// 	[8, 9, 4],
+// 	[7, 6, 5],
+// ];
