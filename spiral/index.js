5c7ec69adad1ec86c5b4645d3b959cf7dfe92ddb
Elijah Mylcev <elijahmylcev@gmail.com>
2022-01-17 22:44:08 +0300

    Example matrix && result

--- spiral/index.js
+++ spiral/index.js
@@ -1,2 +1,14 @@
 const width = +prompt('Ширина матрицы');
 const height = +prompt('Высота матрицы');
+
+function matrix(width, height) {
+	let result = new Array(height).fill().map(() => new Array(width));
+
+	return result;
+}
+
+[
+	[1, 2, 3],
+	[8, 9, 4],
+	[7, 6, 5],
+];
